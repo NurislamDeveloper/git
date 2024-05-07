4450292b0df56d0ae695f5a51c234c65178a4a87
Nurislam <nurislam.temirbekov.bj@gmail.com>
2024-05-07 20:14:39 +0600

    ughegio

---
+++ fast_food/lib/presentetions/constans/style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static const styleOFAppbar = TextStyle(
+    color: Colors.white,
+    fontSize: 30,
+    fontWeight: FontWeight.w800,
+  );
+  static const styleForMenue = TextStyle(
+    color: Colors.white,
+    fontSize: 25,
+    fontWeight: FontWeight.w400,
+  );
+}
--- fast_food/lib/presentetions/pages/main_page.dart
+++ fast_food/lib/presentetions/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:fast_food/presentetions/constans/style.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -11,7 +12,29 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.brown,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "My App",
+          style: Style.styleOFAppbar,
+        ),
+      ),
+      body: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            Card(
+              color: Colors.grey.shade200,
+              child:  Row(
+                children: [
+                  Image.asset('assets/pizza.png'),
+                  const Text("Hamburger",style:Style.styleForMenue,),
+                ], 
+              ),
+            );
+          },
+        ),
     );
   }
 }
